c6777b3215007dc5b305a09644ee644cf83b456d
Michiel <michielvaneester@gmail.com>
2021-08-09 23:06:52 +0200

    startup

---
+++ CollatzConjecture/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CollatzConjecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("The Collatz Conjecture:");
+            Console.WriteLine("Uneven number: *3+1");
+            Console.WriteLine("Even number: /2");
+            Console.WriteLine("Choose a number:");
+        }
+    }
+}
